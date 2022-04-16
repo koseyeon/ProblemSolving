c64f51890c2854333d95ea77556ee604d1fb1421
Seyeon Ko <82452337+koseyeon@users.noreply.github.com>
2022-04-16 23:15:01 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = list(map(int,input().split()))
+dp = a[:]
+for i in range(len(a)):
+    for j in range(i):
+        if a[j]<a[i]:
+            dp[i] = max(dp[i],dp[j]+a[i])
+print(max(dp))
