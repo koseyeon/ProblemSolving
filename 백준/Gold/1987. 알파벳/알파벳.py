4b7a4e20c1136680d70d0328a7acce0a1c097072
Seyeon Ko <82452337+koseyeon@users.noreply.github.com>
2022-04-23 14:13:45 +0900

    [Gold IV] Title: 알파벳, Time: 200 ms, Memory: 115136 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,26 +1,25 @@
-def cango(x,y,s):
-  if 0<=x<r and 0<=y<c and not (s & 1<<ord(grid[x][y])-65):
-    return True
-  return False
-def dfs(x,y,state,cnt):
-  global ans
-  s = [[x,y,state,cnt]]
-  while s:
-    x,y,state,cnt = s.pop()
-    ans = max(ans,cnt)
-    if cnt == 26:
-      return
+R,C = map(int,input().split())
+
+arr = [list(input()) for _ in range(R)]
+check = [['']*C for _ in range(R)]
+
+stack = [(0,0,1,arr[0][0])]
+result = 0
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+while stack:
+    x,y,cnt,total = stack.pop()
+    if result < cnt:
+        result = cnt
+    if result == 26:
+        break
     for i in range(4):
-      nx,ny = x+dx[i],y+dy[i]
-      if cango(nx,ny,state):
-        ns = state | (1<<ord(grid[nx][ny])-65)
-        s.append([nx,ny,ns,cnt+1])
-r,c = map(int,input().split())
-grid = []
-for _ in range(r):
-  grid.append(list(input()))
-ans = 0
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-dfs(0,0,1<<(ord(grid[0][0])-65),1)
-print(ans)
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < R and 0<= ny <C:
+            if arr[nx][ny] not in total:
+                temp = total + arr[nx][ny]
+                if check[nx][ny] != temp:
+                    check[nx][ny] = temp
+                    stack.append((nx,ny,cnt+1,temp))
+print(result)
