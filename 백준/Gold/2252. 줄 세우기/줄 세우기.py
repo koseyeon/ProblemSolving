e40f17e984bbdd879b9f1ebcfdf2ea0f5ad80363
Seyeon Ko <82452337+koseyeon@users.noreply.github.com>
2022-04-23 16:28:44 +0900

    [Gold III] Title: 줄 세우기, Time: 240 ms, Memory: 122976 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,20 +1,20 @@
-from collections import deque
 n,m = map(int,input().split())
 graph = [[] for _ in range(n+1)]
 degree = [0]*(n+1)
 for _ in range(m):
   a,b = map(int,input().split())
-  graph[a].append(b)
-  degree[b]+=1
-d = deque([])
+  graph[b].append(a)
+  degree[a]+=1
+s = []
 for i in range(1,n+1):
   if degree[i]==0:
-    d.append(i)
-ans = ''
-while d:
-  x = d.popleft()
+    s.append(i)
+ans = []
+while s:
+  x = s.pop()
+  ans.append(str(x))
   for next in graph[x]:
     degree[next]-=1
     if degree[next]==0:
-      d.append(next)
-  print(x,end=" ")
+      s.append(next)
+print(' '.join(reversed(ans)))
