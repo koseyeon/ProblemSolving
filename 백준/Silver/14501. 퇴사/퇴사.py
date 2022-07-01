056f01d14ad46f441eed9aca795e49bb41fcb2e2
Seyeon Ko <82452337+koseyeon@users.noreply.github.com>
2022-07-01 18:45:17 +0900

    [Silver III] Title: 퇴사, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -3,9 +3,7 @@ dp = [0]*(n+1)
 work = []
 for i in range(n):
     work.append(list(map(int,input().split())))
-for i in range(n-1,-1,-1):
-    if i+work[i][0]<=n:
-        dp[i] = max(dp[i+1],dp[i+work[i][0]]+work[i][1])
-    else:
-        dp[i] = max(dp[i],dp[i+1])
-print(dp[0])
+for i in range(n):
+    for j in range(i+work[i][0],n+1):
+        dp[j] = max(dp[j],dp[i]+work[i][1])
+print(dp[n])
