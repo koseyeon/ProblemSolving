e36a51d4bec7945ab539d9458c804817d29e9949
Seyeon Ko <koseyeon97@gmail.com>
2022-10-27 11:29:39 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 4520 ms, Memory: 56740 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,27 @@
+n = int(input())
+parents = [0]*(n+1)
+graph = {}
+for i in range(n-1):
+    a,b = map(int,input().split())
+    if(a not in graph.keys()):
+        graph[a] = [b]
+    else:
+        graph[a].append(b)
+    if(b not in graph.keys()):
+        graph[b] = [a]
+    else:
+        graph[b].append(a)
+stack = [1]
+visited = [0]*(n+1)
+visited[1] = 1
+while stack:
+    parent = stack.pop()
+    children = graph[parent]
+    for child in children:
+        if(visited[child]):
+            continue
+        parents[child] = parent
+        visited[child] = 1
+        stack.append(child)
+for parent in parents[2:]:
+    print(parent)
